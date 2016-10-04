1e720dd82407cc8df6850ec52329775088f41570
kjw\김재우 <kjw900905@naver.com>
2016-10-05 04:07:50 +0900

    트리거

---
+++ Assets/Script/DropBoxSceneScripts/DropBoxSceneCCube.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropBoxSceneCCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other){
+        if(other.tag == "wall")
+        {
+            Debug.Log("aaaa");
+        }
+    }
+}
